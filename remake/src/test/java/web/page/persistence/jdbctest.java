0e8338119aaff8dbb3cd433c6676f837703ab5b9
syrimp <rimyesong@naver.com>
2021-12-10 16:58:06 +0900

    다시 시작...

---
+++ remake/src/test/java/web/page/persistence/jdbctest.java
@@ -0,0 +1,45 @@
+package web.page.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class jdbctest {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		try (Connection conn=
+				DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "dog", "mung")
+				){
+			log.info(conn);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+}
